ec9b7c8aa162c1ba8956ef959ff30b49ba1926a8
Yasuaki Uechi <uetchy@randompaper.co>
2015-08-03 22:42:12 +0900

    Added sync, syncWithLocal, syncWithApm function

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 #!/usr/bin/env node
 
-import {installedPackages} from './lib'
-import {starredPackages} from './lib'
-
-let installed = installedPackages();
-let starred = starredPackages();
-console.log(installed.length + " packages installed")
-console.log(starred.length + " packages starred")
-
-let only_in_apm = diff( setOfName(starred) , setOfName(installed) )
-console.log("# Only in apm");
-console.log(only_in_apm.join(' '));
-
-let only_in_local = diff( setOfName(installed) , setOfName(starred) )
-console.log("\n# Only in local");
-console.log(only_in_local.join(' '));
+import colors from 'colors'
+import * as apm from './lib'
 
 function setOfName(arr) {
   return arr.map((s)=>{
@@ -28,12 +15,77 @@ function diff(a, b) {
   })
 }
 
+function showStatus() {
+  let installed = apm.installedPackages();
+  let starred = apm.starredPackages();
+  console.log(installed.length + " packages installed")
+  console.log(starred.length + " packages starred")
+
+  let only_in_apm = diff( setOfName(starred) , setOfName(installed) )
+  let only_in_local = diff( setOfName(installed) , setOfName(starred) )
+
+  console.log(colors.green("\n# Packages only in apm"));
+  console.log(only_in_apm.join(' '));
+
+  console.log(colors.green("\n# Packages only in local machine"));
+  console.log(only_in_local.join(' '));
+}
+
 function syncWithLocal() {
   // Unstar only_in_apm
   // Star only_in_local
+  let installed = apm.installedPackages();
+  let starred = apm.starredPackages();
+  let only_in_apm = diff( setOfName(starred) , setOfName(installed) )
+  let only_in_local = diff( setOfName(installed) , setOfName(starred) )
+
+  for (let pkg of only_in_apm) {
+    console.log(colors.red("Unstaring ... ") + pkg);
+    apm.unstar(pkg);
+  }
+
+  for (let pkg of only_in_local) {
+    console.log(colors.green("Staring ... ") + pkg);
+    apm.star(pkg);
+  }
 }
 
 function syncWithApm() {
   // Uninstall only_in_local
   // Install only_in_apm
+  let installed = apm.installedPackages();
+  let starred = apm.starredPackages();
+  let only_in_apm = diff( setOfName(starred) , setOfName(installed) )
+  let only_in_local = diff( setOfName(installed) , setOfName(starred) )
+
+  for (let pkg of only_in_local) {
+    console.log(colors.red("Uninstalling ... ") + pkg);
+    apm.uninstall(pkg);
+  }
+
+  for (let pkg of only_in_apm) {
+    console.log(colors.green("Installing ... ") + pkg);
+    apm.install(pkg);
+  }
 }
+
+function sync() {
+  // Install only_in_apm
+  // Star only_in_local
+  let installed = apm.installedPackages();
+  let starred = apm.starredPackages();
+  let only_in_apm = diff( setOfName(starred) , setOfName(installed) )
+  let only_in_local = diff( setOfName(installed) , setOfName(starred) )
+
+  for (let pkg of only_in_apm) {
+    console.log(colors.green("Installing ... ") + pkg);
+    apm.install(pkg);
+  }
+
+  for (let pkg of only_in_local) {
+    console.log(colors.green("Staring ... ") + pkg);
+    apm.star(pkg);
+  }
+}
+
+sync();
